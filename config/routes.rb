69443c30bcf3f3303c13d1bfe3fc520422ea4c40
yoshiki.yamamoto <yoyamamoto113@gmail.com>
2020-03-01 10:41:07 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-    root to: 'tasks#index'
+  root to: 'toppages#index'
 
-get 'signup', to: 'users#new'
-  resources :tasks, only: [:index, :show, :new, :create, :edit, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
