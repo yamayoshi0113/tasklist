30a18be38e2685e4a83dd7c05e4cea4712d350af
yoshiki.yamamoto <yoyamamoto113@gmail.com>
2020-03-01 09:17:13 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     root to: 'tasks#index'
 
-  resources :tasks
+get 'signup', to: 'users#new'
+  resources :tasks, only: [:index, :show, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
